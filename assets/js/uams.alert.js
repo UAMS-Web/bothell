89233b19ad0ede32e86ed8854ea1696d4aff885a
Brent Passmore <bpmore@mac338502.ad.uams.edu>
2016-12-19 15:38:34 -0600

    update

--- assets/js/uams.alert.js
+++ assets/js/uams.alert.js
@@ -1,66 +1,67 @@
-UAMS.Alert = Backbone.View.extend({
+/ Basic UAMS Components
+// --------------
+;UAMS.Alert = Backbone.View.extend({
 
-  alert : '#uamsalert-alert-message',
+ alert : '#uamsalert-alert-message',
 
-  events : {
-    'click .close' : 'hide'
-  },
+ events : {
+   'click .close' : 'hide'
+ },
 
-  template : '<div id="uamsalert-alert-message" class="<% _.each( categories, function( category ) { %> <%= category.slug %> <% }) %>"><div class="container"><span class="close">Close</span><h1><%= title %></h1><p><%= excerpt %><a class="more" href="http://uamsalert.wordpress.com" title="<%= title %>">More info</a></p></div></div>',
+ template : '<div id="uamsalert-alert-message" class="<% _.each( categories, function( category ) { %> <%= category.slug %> <% }) %>"><div class="container"><span class="close">Close</span><h1><%= title %></h1><p><%= excerpt %><a class="more" href="http://uamsalert.wordpress.com" title="<%= title %>">More info</a></p></div></div>',
 
-  initialize  : function( options )
-  {
-    _.bindAll( this, 'render', 'hide' )
-    this.options = _.extend( {}, options )
-    this.model.on( 'sync', this.render )
-  },
+ initialize  : function( options )
+ {
+   _.bindAll( this, 'render', 'hide' )
+   this.options = _.extend( {}, options )
+   this.model.on( 'sync', this.render )
+ },
 
-  render : function()
-  {
-    if ( this.model.get('title'))
-     {
-      $(this.options.after).after( _.template( this.template )( this.model.toJSON() ) )
-      this.setElement( $( this.alert ) )
-    }
-  },
+ render : function()
+ {
+   if ( this.model.get('title'))
+    {
+     $(this.options.after).after( _.template( this.template )( this.model.toJSON() ) )
+     this.setElement( $( this.alert ) )
+   }
+ },
 
-  hide : function()
-  {
-    this.$el.remove()
-  }
+ hide : function()
+ {
+   this.$el.remove()
+ }
 
 })
 
 UAMS.Alert.Model = Backbone.Model.extend({
 
-  alerts :  [
-    'urgent-uams',
-    'alert-uams',
-    'fyi-uams',
-  ],
+ alerts :  [
+   'urgent-uams',
+   'alert-uams',
+   'fyi-uams',
+ ],
 
-  data  : {
-    c : '?',
-    test : true,
-    number:1,
-    type:'post',
-    status:'publish',
-    dataType: 'json'
-  },
+ data  : {
+   c : '?',
+   test : true,
+   number:1,
+   type:'post',
+   status:'publish',
+   dataType: 'json'
+ },
 
-  url :  'https://public-api.wordpress.com/rest/v1/sites/uamsalert.wordpress.com/posts/',
+ url :  'https://public-api.wordpress.com/rest/v1/sites/uamsalert.wordpress.com/posts/',
 
-  initialize : function()
-  {
-    this.fetch( { data : this.data })
-  },
+ initialize : function()
+ {
+   this.fetch( { data : this.data })
+ },
 
-  parse: function(data)
-  {
-    var post = _.first( data.posts )
-    _.extend( post.categories, {alert: { slug : window.location.hash.replace('#','') } } )
-    if ( _.intersection( _.pluck(  post.categories, 'slug' ), this.alerts ).length || post.categories.alert.slug.indexOf( 'uamsalert' ) !== -1 )
-      return post
-  }
-
+ parse: function(data)
+ {
+   var post = _.first( data.posts )
+   _.extend( post.categories, {alert: { slug : window.location.hash.replace('#','') } } )
+   if ( _.intersection( _.pluck(  post.categories, 'slug' ), this.alerts ).length || post.categories.alert.slug.indexOf( 'uamsalert' ) !== -1 )
+     return post
+ }
 });
