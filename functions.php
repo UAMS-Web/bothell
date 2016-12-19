a073c04e08825aa5ad561aa57892873386f580df
Brent Passmore <bpmore@mac338502.ad.uams.edu>
2016-12-19 15:25:16 -0600

    function tweak

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
+    wp_enqueue_script( 'backbone', get_stylesheet_directory_uri() . '/assets/js/backbone.js', array(), '1.0.0', true ); 
     wp_enqueue_script( 'uams-alert-style', get_stylesheet_directory_uri() . '/assets/css/uams.alert.css', array(), '1.0.0', true );
     wp_enqueue_script( 'underscore', get_stylesheet_directory_uri() . '/assets/js/underscore.js', array(), '1.0.0', true );
     wp_enqueue_script( 'site-dev', get_stylesheet_directory_uri() . '/assets/js/site.dev.js', array(), '1.0.0', true );
