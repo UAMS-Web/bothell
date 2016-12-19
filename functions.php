1765b457c95a55c19ce0711fc5eb69da28a348d4
Brent Passmore <bpmore@mac338502.ad.uams.edu>
2016-12-19 15:07:20 -0600

    add js

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@ function theme_enqueue_styles() {
     wp_enqueue_script( 'uams-alert-style', get_stylesheet_directory_uri() . '/assets/css/uams.alert.css', array(), '1.0.0', true );
     wp_enqueue_script( 'underscore', get_stylesheet_directory_uri() . '/assets/js/underscore.js', array(), '1.0.0', true );
     wp_enqueue_script( 'site-dev', get_stylesheet_directory_uri() . '/assets/js/site.dev.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'uams-init', get_stylesheet_directory_uri() . '/assets/js/uams.init.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'uams', get_stylesheet_directory_uri() . '/assets/js/uams.js', array(), '1.0.0', true );
     wp_enqueue_script( 'uams-alert', get_stylesheet_directory_uri() . '/assets/js/uams.alert.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
